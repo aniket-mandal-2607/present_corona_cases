bbf43f164cae224f7ea9ae8628c5006d468880e3
aniket-mandal-2607 <63110858+aniket-mandal-2607@users.noreply.github.com>
2020-04-30 17:27:44 +0530

    Update project.py

--- project.py
+++ project.py
@@ -5,26 +5,27 @@ def y(point,i,color):
     str1="""{}
     Cases: {}
     Deaths: {}
-    Recovered: {}""".format(country['location'][i],country['confirmed'][i],country['dead'][i],country['recovered'][i])
+    Recovered: {}""".format(dataset['location'][i],dataset['confirmed'][i],dataset['dead'][i],dataset['recovered'][i])
     folium.Marker(location=point,
                   popup=str1,
                   icon=folium.Icon(color=color, icon='tint', icon_color='purple')
-    ).add_to(vmap)
+    ).add_to(test)
 
-country=pd.read_csv('countries.csv')
-vmap=folium.Map(location=[0.00,0.00], zoom_start=1)
-coor=country[['latitude','longitude']]
+dataset=pd.read_csv('countries.csv')
+test=folium.Map(location=[0.00,0.00], zoom_start=1)
+places=dataset[['latitude','longitude']]
+place=places.values.tolist()
 i=0
 for point in place:
-    if country['confirmed'][i]>50000:
+    if dataset['confirmed'][i]>50000:
         y(point,i,'darkred')
-    elif country['confirmed'][i]>25000:
+    elif dataset['confirmed'][i]>25000:
         y(point,i,'red')
-    elif country['confirmed'][i]>10000:
+    elif dataset['confirmed'][i]>10000:
         y(point,i,'orange')
     else:
         y(point,i,'green')
         
     i+=1
-
-vmap.save('index.html')
+	
+test.save('index.html')
